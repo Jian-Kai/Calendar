4bc307eb972b1c1df022f7c127e0f2d343ccb075
tony14256 <tony14256@gmail.com>
2019-05-17 21:21:58 +0800

    calendar finish

--- src/Calendar.js
+++ src/Calendar.js
@@ -8,6 +8,22 @@ const Calendar = ({ date, month }) => {
 
     const [current, setCurrent] = useState(0);
     const [focus, setFocus] = useState(0);
+
+    const [arr, setArr] = useState(
+        () => {
+            const col = 6;
+            const row = 7;
+            let temp = [];
+            for (let i = 0; i < col; i++) {
+                let temp2 = [];
+                for (let j = 0; j < row; j++) {
+                    temp2.push(0)
+                }
+                temp.push(temp2)
+            }
+            return temp;
+        }
+    )
     
     // handle month bar moving when it need move
     const moveMon = (value) => {
@@ -28,6 +44,7 @@ const Calendar = ({ date, month }) => {
                     moveMon(value);
                     current === 0 ? setFocus(0) : setFocus(1);
                 }
+                //initDateclick();
                 break;
             case 1:
                 console.log('next')
@@ -37,6 +54,7 @@ const Calendar = ({ date, month }) => {
                     moveMon(value);
                     current + 2 === month.length - 1 ? setFocus(2) : setFocus(1);
                 }
+                //initDateclick();
                 break;
             case 0:
                 console.log('month');
@@ -57,6 +75,27 @@ const Calendar = ({ date, month }) => {
                 break;
             default:
         }
+        initDateclick();
+    }
+
+    const initDateclick = () =>{
+        let newArr = [...arr];
+        for (let i = 0; i < newArr.length; i++) {
+            for (let j = 0; j < newArr[i].length; j++)
+                newArr[i][j] = 0
+        }
+        setArr(newArr);
+    }
+
+    const clickDate = (de, Dr, Dc) => {
+        let newArr = [...arr];
+        for (let i = 0; i < newArr.length; i++) {
+            for (let j = 0; j < newArr[i].length; j++)
+                newArr[i][j] = 0
+        }
+        newArr[Dc][Dr] = 1;
+        setArr(newArr)
+        console.log(arr[Dc][Dr])
     }
 
     // console.log(date[month[current + focus]])
@@ -64,7 +103,7 @@ const Calendar = ({ date, month }) => {
     return <div>
         <Month current={current} focus={focus} month={month} clickMon={clickMon} />
         <Day />
-        <Dates date={date} month={month} current={current} focus={focus} />
+        <Dates date={date} month={month} current={current} focus={focus} arr={arr} clickDate={clickDate}/>
     </div>
 }
 
--- src/Dates/Dates.js
+++ src/Dates/Dates.js
@@ -1,103 +1,72 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Dates.css'
 import moment from 'moment';
 
 import InDate from './Date';
 
-const Dates = ({ date, month, focus, current }) => {
-    const [arr, setArr] = useState(
-        () => {
-            const col = 6;
-            const row = 7;
-            let temp = [];
-            for (let i = 0; i < col; i++) {
-                let temp2 = [];
-                for (let j = 0; j < row; j++) {
-                    temp2.push(0)
-                }
-                temp.push(temp2)
-            }
-            return temp;
-        }
-    )
+const Dates = ({ date, month, focus, current, arr, clickDate }) => {
+
     const width = (document.body.clientWidth) / 7;
 
     const thisMon = date[month[current + focus]];
     let calendarArr = [];
-
+    for (let i = 0; i < thisMon.length; i++) {
+        let found = calendarArr.find((trip) => trip.date === thisMon[i].date);
+        if (!found)
+            calendarArr.push(thisMon[i])
+        else {
+            let index = calendarArr.indexOf(found);
+            if (thisMon[i].price < found.price)
+                calendarArr[index] = found
+        }
+    }
+    calendarArr.sort((a, b) => a.date > b.date ? 1 : -1)
+    // console.log(thisMon)
+    // console.log(calendarArr)
     let Y = moment(new Date(thisMon[0].date)).format("YYYY");
     let M = moment(new Date(thisMon[0].date)).format("MM");
     const firstDay = moment(new Date(Y + '/' + M + '/01')).day();
     const start = moment([Y, M - 1]);
     const final = moment(start).endOf('month').toDate().getDate() + firstDay;
-    let count = 1;
-    const col = 6;
-    const row = 7;
-    for (let i = 0; i < col; i++) {
-        let temp = [];
-        for (let j = 0; j < row; j++) {
-            let detial = null;
-            if (i * 7 + j >= firstDay && i * 7 + j < final) {
-                temp.push({
-                    detial: detial,
-                    pos: count,
-                    clicked: false
-                });
-                count++;
-            }
-            else temp.push({
-                detial: detial,
-                pos: 0,
-                clicked: false
-            })
-        }
-        calendarArr.push(temp);
-    }
 
-    for (let i = 0; i < thisMon.length; i++) {
-        let thisDate = moment(thisMon[i].date).date() + firstDay - 1;
-        let c = Math.floor(thisDate / 7);
-        let r = thisDate % 7;
-        calendarArr[c][r].detial = thisMon[i];
-    }
-
+    let count = 0;
     const Sdetial = (deti, r, c) => {
-        const click = (de, Dr, Dc) => {
-            let newArr = [...arr];
-            for(let i = 0; i < newArr.length; i++){
-                for(let j = 0; j < newArr[i].length; j++)
-                newArr[i][j] = 0
-            }
-            newArr[Dc][Dr] = 1;
-            setArr(newArr)
-            console.log(arr[Dc][Dr])
-        }
+        if(count < calendarArr.length - 1)
+            count++;
         if (deti !== null) {
-            return <InDate deti={deti} row={r} col={c} click={click} />
+            return <InDate deti={deti} row={r} col={c} click={clickDate} />
         }
     }
 
     return <div className='dateLayout'>
         {
-            calendarArr.map((cols, colid) => {
+            arr.map((cols, colid) => {
                 return <div key={colid} className='week'>
                     {
                         cols.map((rows, rowid) => {
                             let classD = (rowid === 6) ? 'date sat' : 'date';
-                            if (rows.pos > 0) classD += ' exist';
+                            if (colid * 7 + rowid >= firstDay && colid * 7 + rowid < final)
+                                classD += ' exist'
+                            // if (rows.pos > 0) classD += ' exist';
                             if (arr[colid][rowid] === 1) classD += ' clicked';
                             return (
-                                (rows.pos === 0) ?
-                                    <div key={rowid} className={classD} style={{ width: width }}></div>
-                                    :
-                                    <div key={rowid} className={classD} style={{ width: width }}>
-                                        <span className='num'>
-                                            {rows.pos}
-                                        </span>
-                                        {
-                                            Sdetial(rows.detial, rowid, colid)
-                                        }
-                                    </div>
+
+                                <div key={rowid} className={classD} style={{ width: width }}>
+                                    {
+                                        (colid * 7 + rowid >= firstDay && colid * 7 + rowid < final) ?
+                                            <span className='num'>
+                                                {colid * 7 + rowid - firstDay + 1}
+                                            </span>
+                                            :
+                                            ''
+                                    }
+                                    {
+                                        (colid * 7 + rowid - firstDay + 1 === moment(new Date(calendarArr[count].date)).date()) ?
+                                            Sdetial(calendarArr[count], rowid, colid)
+                                            :
+                                            ''
+                                    }
+                                </div>
                             )
                         })
                     }
